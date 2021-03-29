b0f72a085b95d0e60f179b3d3cda1c0a33afed9d
ND <aj.nglun@gmail.com>
2021-03-29 23:47:54 +0400

    adding js plugins

---
+++ main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function () {
+  var mouseX = 0,
+    mouseY = 0;
+  var xp = 0,
+    yp = 0;
+
+  $(document).mousemove(function (e) {
+    mouseX = e.pageX - 2.5;
+    mouseY = e.pageY - 2.5;
+  });
+
+  setInterval(function () {
+    xp += (mouseX - xp) / 6;
+    yp += (mouseY - yp) / 6;
+    $("#circle").css({ left: xp + "px", top: yp + "px" });
+  }, 20);
+});
+
+gsap.from(".phone1", {
+  duration: 1.5,
+  opacity: 0,
+  scale: 0.3,
+  ease: "back",
+});
