863c0e62afa53b6e1855b2222bc955748274a8c3
ND <aj.nglun@gmail.com>
2021-04-15 20:23:53 +0400

    Added animation in masthead

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Setting the cursor as a touch figure like on smartphones
 jQuery(document).ready(function () {
   var mouseX = 0,
     mouseY = 0;
@@ -16,11 +17,13 @@ jQuery(document).ready(function () {
   }, 20);
 });
 
-
-//First Demo phone animation (pop animation)
+//animation for picture in masthead (pop animation)
 gsap.from(".phone1", {
   duration: 1.5,
   opacity: 0,
   scale: 0.3,
   ease: "back",
 });
+
+//animation for scroll arrow
+gsap.to(".scroll-image", { y: 20, duration: 2.5, repeat: 5, ease: "elastic" });
