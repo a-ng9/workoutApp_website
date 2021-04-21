27eec92bf309ee76b16be97e0efa54428c41ee9d
ND <aj.nglun@gmail.com>
2021-04-21 18:41:31 +0400

    Added scroll animations

--- main.js
+++ main.js
@@ -19,7 +19,32 @@ jQuery(document).ready(function () {
 
 gsap.registerPlugin(ScrollTrigger);
 
-//animation for picture in masthead (pop animation)
+// Snapping scroll animation to different sections
+function goToSection(i, anim) {
+  gsap.set("body", { overflow: "hidden" });
+
+  gsap.to("body", {
+    ease: "power",
+    scrollTo: { y: i * innerHeight },
+    duration: 1.4,
+    // overwrite: true,
+    onComplete: () => gsap.set("body", { overflow: "auto" }),
+  });
+}
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    onEnter: () => goToSection(i),
+  });
+
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "bottom bottom",
+    onEnterBack: () => goToSection(i),
+  });
+});
+
+//Animation for picture in masthead (pop animation)
 gsap.from(".masthead-image", {
   duration: 1.5,
   opacity: 0,
@@ -27,44 +52,107 @@ gsap.from(".masthead-image", {
   ease: "back",
 });
 
-//animation for scroll-ARROW in masthead
+//Animation for scroll-ARROW in masthead
 gsap.to(".scroll-image", { y: 15, duration: 2, repeat: -1, ease: "elastic" });
 
-//scroll animation triggers for Main Features section
+//Mainfeatures pop-in animation
 gsap.from("#section-mainFeatures", {
   scrollTrigger: {
     // markers: true,
     start: "20% 60% ",
+    end: "55% 40%",
     trigger: "#section-mainFeatures",
-    toggleActions: "play none none none",
+    toggleActions: "play none none reverse",
   },
+  ease: "power",
+  y: 100,
   duration: 2,
   opacity: 0,
 });
 
+//Animation for how-it-works section
+//Step 1
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#step1",
+      start: "-25% 60%", //Starts animation
+      end: "top 25%", //Ends animation
+      // start:"center center",
+      // end:"bottom top",
+      // markers: true,
+      scrub: 3,
+      duration: 10,
+    },
+  })
+  .from("#num1", { x: innerWidth * 1 })
+  .from("#demo1", { x: innerWidth * 1 })
+  .from("#HIW-title1", { x: innerWidth * 1 })
+  .from("#HIW-text1", { x: innerWidth * 1 });
+
+//Step 2
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#step2",
+      start: "-25% 60%", //Starts animation
+      end: "10% 30%", //Ends animation
+      // markers: true,
+      scrub: 3,
+    },
+  })
+  .from("#demo2", { x: innerWidth * -1 })
+  .from("#num2", { x: innerWidth * -1 })
+  .from("#HIW-title2", { x: innerWidth * -1 })
+  .from("#HIW-text2", { x: innerWidth * -1 });
+
+  //Step 3
+  gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#step3",
+      start: "-25% 60%", //Starts animation
+      end: "10% 30%", //Ends animation
+      // markers: true,
+      scrub: 3,
+    },
+  })
+  .from("#demo3", { x: innerWidth * -1 })
+  .from("#num3", { x: innerWidth * -1 })
+  .from("#HIW-title3", { x: innerWidth * -1 })
+  .from("#HIW-text3", { x: innerWidth * -1 });
+
+  // Step 4
+  gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#step4",
+      start: "-25% 60%", //Starts animation
+      end: "10% 30%", //Ends animation
+      // markers: true,
+      scrub: 3,
+    },
+  })
+  .from("#demo4", { x: innerWidth * -1 })
+  .from("#num4", { x: innerWidth * -1 })
+  .from("#HIW-title4", { x: innerWidth * -1 })
+  .from("#HIW-text4", { x: innerWidth * -1 });
+
+  // Step 5
+  gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#step5",
+      start: "-25% 60%", //Starts animation
+      end: "10% 30%", //Ends animation
+      // markers: true,
+      scrub: 3,
+    },
+  })
+  .from("#demo5", { x: innerWidth * -1 })
+  .from("#num5", { x: innerWidth * -1 })
+  .from("#HIW-title5", { x: innerWidth * -1 })
+  .from("#HIW-text5", { x: innerWidth * -1 });
+
 // Source to check scroll animation
 // https://greensock.com/docs/v3/Plugins/ScrollTrigger
-
-function goToSection(i, anim) {
-  gsap.set("body", {overflow: "hidden"});
-  
-  gsap.to("body", {
-    scrollTo: {y: i*innerHeight, autoKill: false},
-    duration: 1.4,
-    overwrite: true,
-    onComplete: () => gsap.set("body", {overflow: "auto"})
-  });
-}
-
-gsap.utils.toArray(".panel").forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    onEnter: () => goToSection(i)
-  });
-  
-  ScrollTrigger.create({
-    trigger: panel,
-    start: "bottom bottom",
-    onEnterBack: () => goToSection(i)
-  });
-});
